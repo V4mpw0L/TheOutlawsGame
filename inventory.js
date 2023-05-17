08cdcfb5eb055ed39f6f9eaa0bef3bd4e2b3a69b
V4mpw0L <125161567+V4mpw0L@users.noreply.github.com>
2023-05-16 18:54:08 -0700

    Update inventory.js

--- inventory.js
+++ inventory.js
@@ -84,9 +84,14 @@ function updateInventory() {
 const itemName = notification.querySelector('#item-name');
 const itemDescription = notification.querySelector('#item-description');
 const itemAttributes = notification.querySelector('#item-attributes');
+String.prototype.capitalize = function() {
+  return this.charAt(0).toUpperCase() + this.slice(1);
+};
+
 const itemRarity = notification.querySelector('#item-rarity');
 itemRarity.textContent = item.rarity;
 itemRarity.classList.add(`rarity-${item.rarity.toLowerCase()}`);
+
 
 const sellButton = notification.querySelector('#sell-button');
 const equipButton = notification.querySelector('#equip-button');
